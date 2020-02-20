e9d01c92e7659966bb2454eedcf2a56af430e147
meas9developer <60771110+meas9developer@users.noreply.github.com>
2020-02-20 15:59:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 const greeter = require('./lib/greeter');
 
-exports.handler = function(event, context) {
+exports.lambda_handler = function(event, context) {
     console.log('Event:', JSON.stringify(event));
     
     const greeting = greeter.greet(event.name);
