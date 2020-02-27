c9e4edb39a04ce145a8162365ce4560d7f931b21
EC2 Default User <ec2-user@ip-172-31-29-35.us-west-2.compute.internal>
2020-02-27 03:44:16 +0000

    64

--- src/components/__tests__/LapButton-test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import LapButton from './../LapButton'
-import renderer from 'react-test-renderer'
-
-it('renders without crashing', () => {
-  const rendered = renderer.create(<LapButton />).toJSON()
-  expect(rendered).toBeTruthy()
-  expect(rendered).toMatchSnapshot()
-});
--- src/components/__tests__/LapInfo-test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import LapInfo from './../LapInfo'
-import renderer from 'react-test-renderer'
-
-it('renders without crashing', () => {
-  const rendered = renderer.create(<LapInfo />).toJSON()
-  expect(rendered).toBeTruthy()
-  expect(rendered).toMatchSnapshot()
-});
